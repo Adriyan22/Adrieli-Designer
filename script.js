b5aebc440228f396588025cf7ae4ec2816833cb5
Adriyan Web Developer <145672511+Adriyan22@users.noreply.github.com>
2024-11-27 14:21:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuMobile = document.querySelector('.menu-nav');
+
+function exibirEocultar() {
+    menuMobile.classList.toggle('menu-nav')
+    menuMobile.classList.toggle('menu-mobile')
+}
+
+function exibirMensagemDeBoasVindas() {
+    alert("Que bom que você chegou! Vamos cuidar das suas sombrancelhas com todo carinho ")
+}
+
+
+
